c739d907a17c525814c89c925513fe9ce4bd724d
Vlad <vladhill69@gmail.com>
2025-12-19 12:44:11 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+// Basic Cache for Offline
+const CACHE_NAME = 'plant-shop-v1';
+const urlsToCache = ['index.html', 'shop.html', 'styles.css', 'script.js'];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
